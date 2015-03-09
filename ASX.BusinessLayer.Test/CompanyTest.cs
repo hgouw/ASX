c9458f989225d6d5f4a3a6b78869cf277b2e4809
Herman Gouw <hermangouw@gmail.com>
2015-03-10 10:13:27 +1100

    Replaced BusinessLayer.Tests with BusinessLayer.Test

--- ASX.BusinessLayer.Test/CompanyTest.cs
+++ ASX.BusinessLayer.Test/CompanyTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ASX.BusinessLayer.Tests
+namespace ASX.BusinessLayer.Test
 {
     [TestClass]
     public class CompanyTest
