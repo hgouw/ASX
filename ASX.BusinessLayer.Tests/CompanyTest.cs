65644ed93b89d20ae6e63addd599adad1ad4cdf2
Herman Gouw <hermangouw@gmail.com>
2015-03-10 07:18:39 +1100

    Added ASX.BusinessLayer.Tests

---
+++ ASX.BusinessLayer.Tests/CompanyTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASX.BusinessLayer.Tests
+{
+    [TestClass]
+    public class CompanyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
