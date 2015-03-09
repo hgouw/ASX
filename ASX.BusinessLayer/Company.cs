944579590d34f0987b55a57349d64bf4a37d46b0
Herman Gouw <hermangouw@gmail.com>
2015-03-09 11:43:54 +1100

    Added Company and History classes

---
+++ ASX.BusinessLayer/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace ASX.BusinessLayer
+{
+    public class Company
+    {
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ ASX.BusinessLayer/History.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ASX.BusinessLayer
+{
+    public class History
+    {
+        public DateTime Date { get; set; }
+        public string Code { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Last { get; set; }
+    }
+}
