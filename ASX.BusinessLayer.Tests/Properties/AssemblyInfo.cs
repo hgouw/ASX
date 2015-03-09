96322c42597bf6ef72f3b70db702109d0b89455d
Herman Gouw <hermangouw@gmail.com>
2015-03-10 09:49:06 +1100

    Added gouw.ws in the Copyright

--- ASX.BusinessLayer.Tests/Properties/AssemblyInfo.cs
+++ ASX.BusinessLayer.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ASX.BusinessLayer.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © gouw.ws 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
