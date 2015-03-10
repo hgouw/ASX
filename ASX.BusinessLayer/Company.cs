425cddeea4556bb043eaf3ec6142eeeffd436b41
Herman Gouw <hermangouw@gmail.com>
2015-03-10 16:08:49 +1100

    Added the methods Retrieve, Save and Validate

--- ASX.BusinessLayer/Company.cs
+++ ASX.BusinessLayer/Company.cs
@@ -4,5 +4,20 @@
     {
         public string Code { get; private set; }
         public string Name { get; private set; }
+
+        public Company Retrieve(string code)
+        {
+            return new Company();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            return false;
+        }
     }
 }
--- ASX.BusinessLayer/History.cs
+++ ASX.BusinessLayer/History.cs
@@ -10,5 +10,20 @@ namespace ASX.BusinessLayer
         public decimal High { get; set; }
         public decimal Low { get; set; }
         public decimal Last { get; set; }
+
+        public History Retrieve(DateTime date, string Code)
+        {
+            return new History();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            return true;
+        }
     }
 }
