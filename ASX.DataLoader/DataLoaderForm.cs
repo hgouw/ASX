0a36b41d96bf1a3bf38abebe26e81eb84026f182
Herman Gouw <hermangouw@gmail.com>
2015-03-12 06:58:50 +1100

    More changes

--- ASX.DataLoader/DataLoaderForm.cs
+++ ASX.DataLoader/DataLoaderForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,16 @@ namespace ASX.DataLoader
 {
     public partial class DataLoaderForm : Form
     {
+        FileSystemWatcher _watcher;
+
         public DataLoaderForm()
         {
             InitializeComponent();
+            ApplySettings();
+        }
+
+        private void ApplySettings()
+        {
         }
     }
 }
