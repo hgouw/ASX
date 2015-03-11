a93a53eac627a327da65401b9ee4ffd308278911
Herman Gouw <hermangouw@gmail.com>
2015-03-11 20:15:16 +1100

    Added more unit tests

---
+++ ASX.BusinessLayer.Tests/HistoryRepositoryTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ASX.BusinessLayer;
+
+namespace ASX.BusinessLayer.Tests
+{
+    [TestClass()]
+    public class HistoryRepositoryTest
+    {
+        [TestMethod()]
+        public void RetrieveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
