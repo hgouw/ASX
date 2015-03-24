118e5b6762c1e93f1296ef625bf4c8344ae7d5d4
Herman Gouw <hermangouw@gmail.com>
2015-03-24 17:49:20 +1100

    Added ASXModelContext

---
+++ ASX.DataAccess/ASXModelContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASX.DataAccess
+{
+    public class ASXModelContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<History> Historys { get; set; }
+    }
+}
