96cc1af0608c0e22de7a4074e93c005c8b0fc931
Herman Gouw <hermangouw@gmail.com>
2015-04-10 10:41:20 +1000

    Added IRepository

---
+++ ASX.BusinessLayer/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ASX.BusinessLayer
+{
+    public interface IRepository<T>
+    {
+        void Add(T newEntity);
+        void Remove(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
