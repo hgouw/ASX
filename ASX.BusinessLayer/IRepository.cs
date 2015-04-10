71e397f4a5f9fe0910433cfcce9e35ba249e2a19
Herman Gouw <hgouw@MATTPC.nsw.candlerecruit.com>
2015-04-10 16:47:31 +1000

    More changes

--- ASX.BusinessLayer/IRepository.cs
+++ ASX.BusinessLayer/IRepository.cs
@@ -4,10 +4,11 @@ using System.Linq.Expressions;
 
 namespace ASX.BusinessLayer
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : class
     {
         void Add(T newEntity);
         void Remove(T entity);
         IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        IQueryable<T> FindAll();
     }
 }
