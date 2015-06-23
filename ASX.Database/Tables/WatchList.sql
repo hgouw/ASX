7d5e834ec27ef02eb8deb5fdc3c81426528388ec
Herman Gouw <hermangouw@gmail.com>
2015-06-23 20:42:55 +1000

    Cleaned up

--- ASX.Database/Tables/WatchList.sql
+++ ASX.Database/Tables/WatchList.sql
@@ -3,5 +3,4 @@
 	[Code] VARCHAR(10) NOT NULL, 
     CONSTRAINT [PK_WatchList] PRIMARY KEY ([Code]), 
     CONSTRAINT [FK_WatchList_Company] FOREIGN KEY ([Code]) REFERENCES [dbo].[Companies]([Code])
-
 )
