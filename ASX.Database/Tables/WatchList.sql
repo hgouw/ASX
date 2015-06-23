0a911a2a868f7fcae9462c4336216819b2d0ac7d
Herman Gouw <hermangouw@gmail.com>
2015-06-23 10:51:09 +1000

    Added WatchList

---
+++ ASX.Database/Tables/WatchList.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[WatchList]
+(
+	[Code] VARCHAR(10) NOT NULL, 
+    CONSTRAINT [PK_WatchList] PRIMARY KEY ([Code]), 
+    CONSTRAINT [FK_WatchList_Company] FOREIGN KEY ([Code]) REFERENCES [dbo].[Companies]([Code])
+
+)
