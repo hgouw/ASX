1aec56c601043cd78fadaeb68699e470443be256
Herman Gouw <hermangouw@gmail.com>
2015-06-29 21:35:40 +1000

    Extend the length of Name to 100.

--- ASX.Database/Tables/Company.sql
+++ ASX.Database/Tables/Company.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Companies]
 (
     [Code] VARCHAR(10) NOT NULL, 
-    [Name] VARCHAR(50) NOT NULL, 
+    [Name] VARCHAR(100) NOT NULL, 
     [Group] VARCHAR(50) NOT NULL, 
     CONSTRAINT [PK_Company] PRIMARY KEY ([Code]), 
     CONSTRAINT [FK_Company_IndustryGroup] FOREIGN KEY ([Group]) REFERENCES [dbo].[IndustryGroups] ([Group])
