5ce1fd2dbcee722f92ef22d987ac7ef75aaf6e8d
Herman Gouw <hermangouw@gmail.com>
2015-12-29 02:33:00 +1100

    Produced an ASX.Report in CSV

--- ASX.Report/Program.cs
+++ ASX.Report/Program.cs
@@ -10,15 +10,24 @@ namespace ASX.Report
         {
             using (var db = new ASXDbContext())
             {
+                var years = new int[] { 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 };
+                var label = "Code";
+                foreach (var year in years)
+                {
+                    label += "," + year;
+                }
+                Console.WriteLine(label);
+
+                var endOfDays = ASXDbContext.GetEndOfDays();
                 var watchLists = db.WatchLists;
                 foreach (var watchList in watchLists)
                 {
-                    var endOfDays = ASXDbContext.GetEndOfDays();
-                    for (int i = 1997; i <= DateTime.Now.Year; i++)
+                    var line = $"{watchList.Code}";
+                    foreach (var year in years)
                     {
-                        var count = endOfDays.Where(d => d.Code == watchList.Code && d.Date >= new DateTime(i, 1, 1).Date && d.Date <= new DateTime(i, 12, 31).Date).Count();
-                        Console.WriteLine($"{watchList.Code} - {i} - {count}");
+                        line += "," + endOfDays.Where(d => d.Code == watchList.Code && d.Date >= new DateTime(year, 1, 1).Date && d.Date <= new DateTime(year, 12, 31).Date).Count();
                     }
+                    Console.WriteLine(line);
                 }
             }
             Console.ReadLine();
