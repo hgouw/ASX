38dea2e93b5390883549326194758b7cbdf16687
Herman Gouw <hermangouw@gmail.com>
2015-12-29 17:58:32 +1100

    Refactored

--- ASX.DataAccess/ASXDbContext.cs
+++ ASX.DataAccess/ASXDbContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using ASX.BusinessLayer;
@@ -10,8 +9,8 @@ namespace ASX.DataAccess
     {
         public DbSet<IndustryGroup> IndustryGroups { get; set; }
         public DbSet<Company> Companies { get; set; }
-        public DbSet<EndOfDay> EndOfDays { get; set; }
         public DbSet<WatchList> WatchLists { get; set; }
+        public DbSet<EndOfDay> EndOfDays { get; set; }
 
         public static IList<IndustryGroup> GetIndustryGroups()
         {
@@ -29,19 +28,19 @@ namespace ASX.DataAccess
             }
         }
 
-        public static IList<EndOfDay> GetEndOfDays()
-        {
-            using (ASXDbContext dbContext = new ASXDbContext())
-            {
-                return dbContext.EndOfDays.ToList();
-            }
-        }
-
         public static IList<WatchList> GetWatchLists()
         {
             using (ASXDbContext dbContext = new ASXDbContext())
             {
                 return dbContext.WatchLists.ToList();
+            }
+        }
+
+        public static IList<EndOfDay> GetEndOfDays()
+        {
+            using (ASXDbContext dbContext = new ASXDbContext())
+            {
+                return dbContext.EndOfDays.ToList();
             }
         }
     }
