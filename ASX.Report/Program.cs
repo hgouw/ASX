230b43854db6b5839a6213d09891dede71c8ca3a
Herman Gouw <hermangouw@gmail.com>
2016-03-06 15:05:45 +1100

    Updated with static methods

--- ASX.Report/Program.cs
+++ ASX.Report/Program.cs
@@ -8,27 +8,24 @@ namespace ASX.Report
     {
         static void Main(string[] args)
         {
-            using (var db = new ASXDbContext())
+            var years = new int[] { 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 };
+            var label = "Code";
+            foreach (var year in years)
             {
-                var years = new int[] { 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 };
-                var label = "Code";
+                label += "," + year;
+            }
+            Console.WriteLine(label);
+
+            var endOfDays = ASXDbContext.GetEndOfDays();
+            var watchLists = ASXDbContext.GetWatchLists();
+            foreach (var watchList in watchLists)
+            {
+                var line = $"{watchList.Code}";
                 foreach (var year in years)
                 {
-                    label += "," + year;
+                    line += "," + endOfDays.Where(d => d.Code == watchList.Code && d.Date >= new DateTime(year, 1, 1) && d.Date <= new DateTime(year, 12, 31)).Count();
                 }
-                Console.WriteLine(label);
-
-                var endOfDays = ASXDbContext.GetEndOfDays();
-                var watchLists = db.WatchLists;
-                foreach (var watchList in watchLists)
-                {
-                    var line = $"{watchList.Code}";
-                    foreach (var year in years)
-                    {
-                        line += "," + endOfDays.Where(d => d.Code == watchList.Code && d.Date >= new DateTime(year, 1, 1).Date && d.Date <= new DateTime(year, 12, 31).Date).Count();
-                    }
-                    Console.WriteLine(line);
-                }
+                Console.WriteLine(line);
             }
             Console.ReadLine();
         }
