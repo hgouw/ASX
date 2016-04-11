11ee24785ba981989b32aa18a160679b66cb7e55
Herman Gouw <hermangouw@gmail.com>
2016-04-11 17:15:09 +1000

    Added IndustryGroup Controller & View

---
+++ ASX.Web/Controllers/IndustryGroupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASX.DataAccess;
+
+namespace ASX.Web.Controllers
+{
+    public class IndustryGroupController : Controller
+    {
+        // GET: IndustryGroup
+        public ActionResult List()
+        {
+            var industryGroups = ASXDbContext.GetIndustryGroups();
+            ViewBag.IndustryGroups = new SelectList(industryGroups, "Group", "Group");
+            return View();
+        }
+    }
+}
