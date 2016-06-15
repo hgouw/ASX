0396baea80d768072b83156ffeaf37f8dd1b62fe
Herman Gouw <hermangouw@gmail.com>
2016-06-15 17:06:52 +1000

    Added WatchLists

---
+++ ASX.Web/Controllers/WatchListController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using ASX.DataAccess;
+
+namespace ASX.Web.Controllers
+{
+    public class WatchListController : Controller
+    {
+        public ActionResult List()
+        {
+            var watchLists = ASXDbContext.GetWatchLists();
+            ViewBag.WatchLists = new SelectList(watchLists, "WatchList", "WatchList");
+            return View();
+        }
+    }
+}
