2ba083cd6c4309e7cdae4643fa0216bac17dba55
Herman Gouw <hermangouw@gmail.com>
2016-06-29 16:27:27 +1000

    Added Enum

--- ASX.DataLoader/DataLoaderForm.cs
+++ ASX.DataLoader/DataLoaderForm.cs
@@ -11,15 +11,6 @@ using NLog;
 
 namespace ASX.DataLoader
 {
-    public enum OutputType
-    {
-        Debug = 0,
-        Error = 1,
-        Fatal = 2,
-        Info = 3,
-        Trace = 4,
-        Warn = 5
-    }
 
     public partial class DataLoaderForm : Form
     {
---
+++ ASX.DataLoader/Enum.cs
@@ -0,0 +1,12 @@
+﻿namespace ASX.DataLoader
+{
+    public enum OutputType
+    {
+        Debug = 0,
+        Error = 1,
+        Fatal = 2,
+        Info = 3,
+        Trace = 4,
+        Warn = 5
+    }
+}
