98da8521b411c5d67b0f4e72a8a233ab61bb4ab2
Herman Gouw <hermangouw@gmail.com>
2016-06-29 17:05:58 +1000

    Added controls

--- ASX.DataLoader/GenerateScriptForm.Designer.cs
+++ ASX.DataLoader/GenerateScriptForm.Designer.cs
@@ -28,19 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.btnGenerate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Location = new System.Drawing.Point(20, 15);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(32, 13);
+            this.lblStart.TabIndex = 0;
+            this.lblStart.Text = "Start:";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(20, 43);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(29, 13);
+            this.lblEnd.TabIndex = 2;
+            this.lblEnd.Text = "End:";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(58, 12);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(200, 20);
+            this.dtpStart.TabIndex = 1;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(58, 40);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(200, 20);
+            this.dtpEnd.TabIndex = 3;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(105, 75);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.Generate_Click);
             // 
             // GenerateScriptForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 112);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.btnGenerate);
             this.Name = "GenerateScriptForm";
             this.Text = "Generate Script";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Button btnGenerate;
     }
 }
--- ASX.DataLoader/GenerateScriptForm.cs
+++ ASX.DataLoader/GenerateScriptForm.cs
@@ -1,12 +1,43 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using ASX.BusinessLayer;
+using ASX.DataAccess;
+using NLog;
 
 namespace ASX.DataLoader
 {
     public partial class GenerateScriptForm : Form
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        IList<EndOfDay> _endOfDays = null;
+
         public GenerateScriptForm()
         {
             InitializeComponent();
+            LoadDates();
+        }
+
+        private void LoadDates()
+        {
+            try
+            {
+                _endOfDays = ASXDbContext.GetEndOfDays();
+                var lastDate = _endOfDays.OrderByDescending(e => e.Date).First().Date;
+                dtpStart.Value = lastDate.AddDays(1);
+                dtpEnd.Value = DateTime.Today;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to load the dates - {ex.Message}");
+            }
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
