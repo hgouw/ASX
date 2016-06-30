278ff337c592f05e7a7b11bbfbad766d22998961
Herman Gouw <hermangouw@gmail.com>
2016-06-30 10:42:18 +1000

    Implemented Generate Script dialog box

--- ASX.DataLoader/GenerateScriptForm.cs
+++ ASX.DataLoader/GenerateScriptForm.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 using ASX.BusinessLayer;
 using ASX.DataAccess;
@@ -12,7 +14,11 @@ namespace ASX.DataLoader
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
 
+        private const string _sql = "INSERT [ASX].[dbo].[EndOfDays] ([Code], [Date], [Open], [High], [Low], [Close], [Volume]) VALUES (N'{0}', CAST(N'{1}' AS DateTime), CAST({2} AS Decimal(6, 3)), CAST({3} AS Decimal(6, 3)), CAST({4} AS Decimal(6, 3)), CAST({5} AS Decimal(6, 3)), {6})";
+        private const string _filename = "AzureSql_EndOfDays.sql";
+
         IList<EndOfDay> _endOfDays = null;
+        IList<WatchList> _watchLists = null;
 
         public GenerateScriptForm()
         {
@@ -25,6 +31,7 @@ namespace ASX.DataLoader
             try
             {
                 _endOfDays = ASXDbContext.GetEndOfDays();
+                _watchLists = ASXDbContext.GetWatchLists();
                 var lastDate = _endOfDays.OrderByDescending(e => e.Date).First().Date;
                 var startDate = lastDate.AddDays(-1);
                 dtpStart.Value = startDate;
@@ -39,7 +46,43 @@ namespace ASX.DataLoader
 
         private void Generate_Click(object sender, EventArgs e)
         {
-            var endOfDays = _endOfDays.Where(x => x.Date >= dtpStart.Value && x.Date <= dtpEnd.Value).ToList();
+            bool ok = false;
+            try
+            {
+                var sb = new StringBuilder();
+                foreach (var watchList in _watchLists)
+                {
+                    var endOfDays = _endOfDays.Where(x => x.Code == watchList.Code && x.Date >= dtpStart.Value && x.Date <= dtpEnd.Value).ToList();
+                    foreach (var endOfDay in endOfDays)
+                    {
+                        // Code:   {0} - ACX
+                        // Date:   {1} - 2015-12-31 00:00:00.000
+                        // Open:   {2} - 1.910
+                        // High:   {3} - 1.910
+                        // Low:    {4} - 1.800
+                        // Close:  {5} - 1.800
+                        // Volume: {6} - 191455
+                        sb.Append(String.Format(_sql, endOfDay.Code, endOfDay.Date.ToString("yyyy-MM-dd HH:mm:ss.fff"), endOfDay.Open, endOfDay.High, endOfDay.Low, endOfDay.Close, endOfDay.Volume));
+                        sb.Append(Environment.NewLine);
+                    }
+                    sb.Append("GO");
+                    sb.Append(Environment.NewLine);
+                }
+                var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var filePath = Path.Combine(desktopPath, _filename);
+                File.WriteAllText(filePath, sb.ToString());
+                ok = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to generate the script - {ex.Message}");
+                ok = false;
+            }
+            finally
+            {
+                Close();
+                DialogResult = ok ? DialogResult.OK : DialogResult.Abort;
+            }
         }
     }
 }
