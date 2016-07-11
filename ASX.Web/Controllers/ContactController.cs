137afd3e1c558088a9841b7966e1f9c9079fd4c3
Herman Gouw <hermangouw@gmail.com>
2016-07-11 17:21:43 +1000

    Added the code for V2 and V3

--- ASX.Web/Controllers/ContactController.cs
+++ ASX.Web/Controllers/ContactController.cs
@@ -3,6 +3,8 @@ using System.Net.Mail;
 using System.Text;
 using System.Web.Mvc;
 using ASX.Web.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace ASX.Web.Controllers
 {
@@ -21,31 +23,47 @@ namespace ASX.Web.Controllers
             {
                 try
                 {
-                    using (var msg = new MailMessage())
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        sb.Append("Name: " + contact.Name);
-                        sb.Append(Environment.NewLine);
-                        sb.Append("Email: " + contact.Email);
-                        sb.Append(Environment.NewLine);
-                        sb.Append("Phone: " + contact.Phone);
-                        sb.Append(Environment.NewLine);
-                        sb.Append("Message: " + contact.Message);
+                    // V2
+                    //
+                    //String apiKey = Environment.GetEnvironmentVariable("hgouw", EnvironmentVariableTarget.User);
+                    //dynamic sg = new SendGridAPIClient(apiKey);
 
-                        msg.From = new MailAddress(contact.Email.ToString());
-                        msg.To.Add("hermangouw@gmail.com");
-                        msg.Subject = "Contact Me";
-                        msg.IsBodyHtml = false;
-                        msg.Body = sb.ToString();
+                    //Email from = new Email("hermangouw@gmail.com");
+                    //String subject = "Sending with SendGrid is Fun";
+                    //Email to = new Email("hermangouw@gmail.com");
+                    //Content content = new Content("text/plain", "and easy to do anywhere, even with C#");
+                    //Mail mail = new Mail(from, subject, to, content);
 
-                        SmtpClient client = new SmtpClient();
-                        client.Host = "smtp.gmail.com";
-                        client.Port = 465;
-                        client.EnableSsl = true;
-                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        client.Credentials = new System.Net.NetworkCredential("hermangouw@gmail.com", "f3k2fh0OIy?cZ");
-                        client.Send(msg);
-                    }
+                    //dynamic response = sg.client.mail.send.post(requestBody: mail.Get());
+
+                    //
+                    // V3
+                    //
+                    //using (var msg = new MailMessage())
+                    //{
+                    //    StringBuilder sb = new StringBuilder();
+                    //    sb.Append("Name: " + contact.Name);
+                    //    sb.Append(Environment.NewLine);
+                    //    sb.Append("Email: " + contact.Email);
+                    //    sb.Append(Environment.NewLine);
+                    //    sb.Append("Phone: " + contact.Phone);
+                    //    sb.Append(Environment.NewLine);
+                    //    sb.Append("Message: " + contact.Message);
+
+                    //    msg.From = new MailAddress(contact.Email.ToString());
+                    //    msg.To.Add("hermangouw@gmail.com");
+                    //    msg.Subject = "Contact Me";
+                    //    msg.IsBodyHtml = false;
+                    //    msg.Body = sb.ToString();
+
+                    //    SmtpClient client = new SmtpClient();
+                    //    client.Host = "smtp.gmail.com";
+                    //    client.Port = 465;
+                    //    client.EnableSsl = true;
+                    //    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    //    client.Credentials = new System.Net.NetworkCredential("hermangouw@gmail.com", "f3k2fh0OIy?cZ");
+                    //    client.Send(msg);
+                    //}
 
                     return View("Success");
                 }
