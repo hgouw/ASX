9d92499bc451d484f7b59a2d74884796a3fdc48b
Herman Gouw <hermangouw@gmail.com>
2016-07-28 15:30:57 +1000

    Implemented Contact Me

--- ASX.Web/Controllers/ContactController.cs
+++ ASX.Web/Controllers/ContactController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Net.Mail;
-using System.Text;
 using System.Web.Mvc;
 using ASX.Web.Models;
-using SendGrid;
 using SendGrid.Helpers.Mail;
 
 namespace ASX.Web.Controllers
@@ -23,48 +20,14 @@ namespace ASX.Web.Controllers
             {
                 try
                 {
-                    // V2
-                    //
-                    //String apiKey = Environment.GetEnvironmentVariable("hgouw", EnvironmentVariableTarget.User);
-                    //dynamic sg = new SendGridAPIClient(apiKey);
-
-                    //Email from = new Email("hermangouw@gmail.com");
-                    //String subject = "Sending with SendGrid is Fun";
-                    //Email to = new Email("hermangouw@gmail.com");
-                    //Content content = new Content("text/plain", "and easy to do anywhere, even with C#");
-                    //Mail mail = new Mail(from, subject, to, content);
-
-                    //dynamic response = sg.client.mail.send.post(requestBody: mail.Get());
-
-                    //
-                    // V3
-                    //
-                    //using (var msg = new MailMessage())
-                    //{
-                    //    StringBuilder sb = new StringBuilder();
-                    //    sb.Append("Name: " + contact.Name);
-                    //    sb.Append(Environment.NewLine);
-                    //    sb.Append("Email: " + contact.Email);
-                    //    sb.Append(Environment.NewLine);
-                    //    sb.Append("Phone: " + contact.Phone);
-                    //    sb.Append(Environment.NewLine);
-                    //    sb.Append("Message: " + contact.Message);
-
-                    //    msg.From = new MailAddress(contact.Email.ToString());
-                    //    msg.To.Add("hermangouw@gmail.com");
-                    //    msg.Subject = "Contact Me";
-                    //    msg.IsBodyHtml = false;
-                    //    msg.Body = sb.ToString();
-
-                    //    SmtpClient client = new SmtpClient();
-                    //    client.Host = "smtp.gmail.com";
-                    //    client.Port = 465;
-                    //    client.EnableSsl = true;
-                    //    client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    //    client.Credentials = new System.Net.NetworkCredential("hermangouw@gmail.com", "f3k2fh0OIy?cZ");
-                    //    client.Send(msg);
-                    //}
-
+                    var apiKey = "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";
+                    dynamic sendGrid = new SendGrid.SendGridAPIClient(apiKey, "https://api.sendgrid.com");
+                    var from = new Email(contact.Email, contact.Name);
+                    var subject = "Message from hermangouw@net";
+                    var to = new Email("hermangouw@gmail.com", "Herman Gouw");
+                    var content = new Content("text/plain", contact.Message);
+                    var mail = new Mail(from, subject, to, content);
+                    dynamic response = sendGrid.client.mail.send.post(requestBody: mail.Get());
                     return View("Success");
                 }
                 catch (Exception ex)
