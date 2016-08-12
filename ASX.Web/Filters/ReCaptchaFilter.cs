223e988e0d02e8b90fe9f554150151832698523d
Herman Gouw <hermangouw@gmail.com>
2016-08-12 16:52:12 +1000

    Refactored

--- ASX.Web/Filters/ReCaptchaFilter.cs
+++ ASX.Web/Filters/ReCaptchaFilter.cs
@@ -31,8 +31,7 @@ namespace ASX.Web
         {
             try
             {
-                //var request = (HttpWebRequest)WebRequest.Create(String.Format(ConfigurationManager.AppSettings["ReCaptchaUrl"], privateKey, mainResponse));
-                var request = (HttpWebRequest)WebRequest.Create("https://www.google.com/recaptcha/api/siteverify?secret=" + privateKey + "&response=" + mainResponse);
+                var request = (HttpWebRequest)WebRequest.Create(String.Format(ConfigurationManager.AppSettings["ReCaptchaUrl"], privateKey, mainResponse));
                 var response = request.GetResponse();
                 using (var stream = new StreamReader(response.GetResponseStream()))
                 {
