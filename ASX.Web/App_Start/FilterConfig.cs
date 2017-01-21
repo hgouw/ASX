7eafc3c5c7752f82cea37da871799da7822aff6e
Herman Gouw <hermangouw@gmail.com>
2017-01-22 00:51:39 +1100

    Updated the libraries

--- ASX.Web/App_Start/FilterConfig.cs
+++ ASX.Web/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace ASX.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
             filters.Add(new ReCaptchaFilter());
         }
     }
