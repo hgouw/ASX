970fdbec0508f4942fc6f0741c318ccdadcacde6
Herman Gouw <hermangouw@gmail.com>
2017-11-06 16:26:26 +1100

    Implemented sending SMS via Twilio

--- ASX.Web/Controllers/ContactController.cs
+++ ASX.Web/Controllers/ContactController.cs
@@ -3,6 +3,9 @@ using System.Configuration;
 using System.Web.Mvc;
 using ASX.Web.Models;
 using SendGrid.Helpers.Mail;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace ASX.Web.Controllers
 {
@@ -25,6 +28,7 @@ namespace ASX.Web.Controllers
             {
                 try
                 {
+                    // Send email via SendGrid
                     dynamic sendGrid = new SendGrid.SendGridAPIClient(ConfigurationManager.AppSettings["SendGridApiKey"], ConfigurationManager.AppSettings["SendGridUrl"]);
                     var from = new Email(contact.Email, contact.Name);
                     var to = new Email(ConfigurationManager.AppSettings["ToEmailAddress"], ConfigurationManager.AppSettings["ToName"]);
@@ -32,6 +36,12 @@ namespace ASX.Web.Controllers
                     var content = new Content("text/plain", contact.Message);
                     var mail = new Mail(from, subject, to, content);
                     dynamic response = sendGrid.client.mail.send.post(requestBody: mail.Get());
+
+                    // Send SMS via Twilio
+                    var msg = $"From: {contact.Name} ({contact.Email}) - Message: {contact.Message}";
+                    TwilioClient.Init(ConfigurationManager.AppSettings["TwilioAccountSid"], ConfigurationManager.AppSettings["TwilioAuthToken"]);
+                    MessageResource.Create(to: new PhoneNumber(ConfigurationManager.AppSettings["ToMobileNumber"]), from: new PhoneNumber(ConfigurationManager.AppSettings["TwilioMobileNumber"]), body: msg);
+
                     return View("Success");
                 }
                 catch (Exception ex)
