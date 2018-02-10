a279b7c5c4c89fc6de977b67c3faaba37038c1e8
Herman Gouw <hgouw@users.noreply.github.com>
2018-02-10 16:14:24 +1100

    Added EmailEnabled and SMSEnabled

--- ASX.Web/Controllers/ContactController.cs
+++ ASX.Web/Controllers/ContactController.cs
@@ -28,19 +28,25 @@ namespace ASX.Web.Controllers
             {
                 try
                 {
-                    // Send email via SendGrid
-                    dynamic sendGrid = new SendGrid.SendGridAPIClient(ConfigurationManager.AppSettings["SendGridApiKey"], ConfigurationManager.AppSettings["SendGridUrl"]);
-                    var from = new Email(contact.Email, contact.Name);
-                    var to = new Email(ConfigurationManager.AppSettings["ToEmailAddress"], ConfigurationManager.AppSettings["ToName"]);
-                    var subject = ConfigurationManager.AppSettings["Subject"];
-                    var content = new Content("text/plain", contact.Message);
-                    var mail = new Mail(from, subject, to, content);
-                    dynamic response = sendGrid.client.mail.send.post(requestBody: mail.Get());
+                    if (Convert.ToBoolean(ConfigurationManager.AppSettings["EmailEnabled"]))
+                    {
+                        // Send email via SendGrid
+                        dynamic sendGrid = new SendGrid.SendGridAPIClient(ConfigurationManager.AppSettings["SendGridApiKey"], ConfigurationManager.AppSettings["SendGridUrl"]);
+                        var from = new Email(contact.Email, contact.Name);
+                        var to = new Email(ConfigurationManager.AppSettings["ToEmailAddress"], ConfigurationManager.AppSettings["ToName"]);
+                        var subject = ConfigurationManager.AppSettings["Subject"];
+                        var content = new Content("text/plain", contact.Message);
+                        var mail = new Mail(from, subject, to, content);
+                        dynamic response = sendGrid.client.mail.send.post(requestBody: mail.Get());
+                    }
 
-                    // Send SMS via Twilio
-                    var msg = $"From: {contact.Name} ({contact.Email}) - Message: {contact.Message}";
-                    TwilioClient.Init(ConfigurationManager.AppSettings["TwilioAccountSid"], ConfigurationManager.AppSettings["TwilioAuthToken"]);
-                    MessageResource.Create(to: new PhoneNumber(ConfigurationManager.AppSettings["ToMobileNumber"]), from: new PhoneNumber(ConfigurationManager.AppSettings["TwilioMobileNumber"]), body: msg);
+                    if (Convert.ToBoolean(ConfigurationManager.AppSettings["SMSEnabled"]))
+                    {
+                        // Send SMS via Twilio
+                        var msg = $"From: {contact.Name} ({contact.Email}) - Message: {contact.Message}";
+                        TwilioClient.Init(ConfigurationManager.AppSettings["TwilioAccountSid"], ConfigurationManager.AppSettings["TwilioAuthToken"]);
+                        MessageResource.Create(to: new PhoneNumber(ConfigurationManager.AppSettings["ToMobileNumber"]), from: new PhoneNumber(ConfigurationManager.AppSettings["TwilioMobileNumber"]), body: msg);
+                    }
 
                     return View("Success");
                 }
