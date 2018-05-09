7a84f3a6c38f6c786eca406f5647e202e6e5cb86
Herman Gouw <hermangouw@gmail.com>
2018-05-09 15:01:21 +1000

    Added DataAccessLayer

--- ASX.Api/EndOfDays.cs
+++ ASX.Api/EndOfDays.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using ASX.DataAccessLayer;
 
 namespace ASX.Api
 {
@@ -18,8 +19,17 @@ namespace ASX.Api
             TraceWriter log)
         {
             log.Info("Received EndOfDays request");
+
             HttpResponseMessage response;
-            var company = req.GetQueryNameValuePairs().FirstOrDefault(q => string.Compare(q.Key, "company", true) == 0).Value;
+
+            using (var db = new ASXEntities())
+            {
+                log.Info("Try to get the list of Companies");
+                var companies = db.Companies.ToList();
+                log.Info("Successfully get the list of Companies");
+            }
+
+            var company = "CPU"; //req.GetQueryNameValuePairs().FirstOrDefault(q => string.Compare(q.Key, "company", true) == 0).Value;
             var from = req.GetQueryNameValuePairs().FirstOrDefault(q => string.Compare(q.Key, "from", true) == 0).Value;
             var to = req.GetQueryNameValuePairs().FirstOrDefault(q => string.Compare(q.Key, "to", true) == 0).Value;
             if (company != null)
