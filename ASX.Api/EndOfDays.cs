02ad1eda0261a8891ff4be86c68adc21df0235cb
Herman Gouw <hermangouw@gmail.com>
2018-05-10 14:42:46 +1000

    Refactored

--- ASX.Api/EndOfDays.cs
+++ ASX.Api/EndOfDays.cs
@@ -2,11 +2,9 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-using ASX.BusinessLayer;
 using ASX.DataAccess;
 
 namespace ASX.Api
