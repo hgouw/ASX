0c9167036ed9dc2c319f0ede5b0879f04f618b1e
Herman Gouw <hgouw@users.noreply.github.com>
2018-09-05 00:10:58 +1000

    Added GoogleChart

---
+++ ASX.Web/Helpers/WebHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace System.Web.Helpers
+{
+    public class GoogleChart
+    {
+        public List<SharePrice> SharePrices { get; set; }
+    }
+
+    public class SharePrice
+    {
+        public DateTime Date { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
