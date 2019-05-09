2b82aab5939c022a90f2884b0c2753eff425354c
Herman Gouw <hermangouw@gmail.com>
2019-05-09 11:56:06 +1000

    Added script files to update Companies, WatchLists and EndOfDays tables

---
+++ Scipts/Update Companies (APT, KGN, Z1P).sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Companies] ([Code], [Name], [Group]) VALUES (N'APT', N'AFTERPAY TOUCH GROUP LIMITED', N'Software & Services')
+INSERT INTO [dbo].[Companies] ([Code], [Name], [Group]) VALUES (N'KGN', N'KOGAN.COM LTD', N'Retailing')
+INSERT INTO [dbo].[Companies] ([Code], [Name], [Group]) VALUES (N'Z1P', N'ZIP CO LIMITED', N'Diversified Financials')
---
+++ Scipts/Update WatchLists (APT, KGN, Z1P).sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[WatchLists] ([Code]) VALUES (N'APT')
+INSERT INTO [dbo].[WatchLists] ([Code]) VALUES (N'KGN')
+INSERT INTO [dbo].[WatchLists] ([Code]) VALUES (N'Z1P')
