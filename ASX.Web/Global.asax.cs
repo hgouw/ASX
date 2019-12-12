6622639a76f0153b8bffb076d039476b4ac94d84
Herman Gouw <hermangouw@gmail.com>
2019-12-12 17:56:30 +1100

    Added DateTimeModelBinder

--- ASX.Web/Global.asax.cs
+++ ASX.Web/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ASX.Web
@@ -10,6 +13,20 @@ namespace ASX.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(DateTime?), new DateTimeModelBinder());
+        }
+    }
+
+    public class DateTimeModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (value == null) return value;
+
+            var result = new DateTime();
+            DateTime.TryParse(value.AttemptedValue, CultureInfo.GetCultureInfo("en-AU"), DateTimeStyles.None, out result);
+            return result;
         }
     }
 }
